4dce96337d2fe2bd81eee3f9c3ca9125a95370c2
tanatana <aiueo2003haru@gmail.com>
2026-01-25 23:51:45 +0900

    ターゲット追加

---
+++ Source/Run/RunPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RunPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUN_API ARunPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
